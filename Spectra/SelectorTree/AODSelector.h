ca97bdbf370c46bdb58dcadba7868c5240c8d6b6
mpuccio <maximiliano.puccio@cern.ch>
2015-02-11 00:11:27 +0100

    Different class of centralities for deuterons

--- Spectra/SelectorTree/AODSelector.h
+++ Spectra/SelectorTree/AODSelector.h
@@ -22,7 +22,7 @@ class TF1;
 #define EPSILON 1E-16
 #define MD 1.875612859f
 #define M2D MD*MD
-#define kNCent 5
+#define kNCent 3
 #define kNBins 28
 #define kNBinsTPC 3
 #define kNDCABins 10
@@ -96,7 +96,7 @@ class AODSelector : public TSelector {
     };
     for (int i = 0; i < kNBins + 1; ++i) fBins[i] = bins[i];
     
-    double cent[kNCent + 1] = {0.,10.,20.,40.,60.,80.};
+    double cent[kNCent + 1] = {0.,10.,30.,50.};
     for (int i = 0; i < kNCent + 1; ++i) fCentralityClasses[i] = cent[i];
     
     fCorrectionAD.SetParameters(-2.10154e-03,-4.53472e-01,-3.01246e+00);
