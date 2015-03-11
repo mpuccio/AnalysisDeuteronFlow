9cfd96913bb8bff3a3b913b1b97ca8ec84edfd5f
mpuccio <maximiliano.puccio@cern.ch>
2015-03-11 14:42:49 +0100

    Ready to test with more cuts

--- Spectra/SelectorTree/RunSelector.C
+++ Spectra/SelectorTree/RunSelector.C
@@ -21,23 +21,32 @@
     }
   }
 
-  const double kCent[5] = {0.,10.,20.,40.,60.};
+  const double kCent[4] = {0.,10.,30.,50.};
   const double kBins[29] = {
     0.4f,0.5f,0.6f,0.7f,0.8f,0.9f,1.0f,1.1f,1.2f,1.4f,
     1.6f,1.8f,2.0f,2.2f,2.4f,2.6f,2.8f,3.0f,3.2f,3.4f,
     3.6f,3.8f,4.0f,4.2f,4.4f,5.0f,6.0f,8.0f,10.f
   };
   TH2F *hBins = new TH2F("hBins","hBins",4,kCent,28,kBins);
-  TH1D *hCuts = new TH1D("hCuts","hCuts",9,0,9);
-  enum cutsName {kEtaMin=1,kEtaMax,kYMin,kYMax,kTPCsig,kTPCchi2,kSPDrec,kDCAxy,kDCAz};
-  double cutsA[9] = {-0.8,0.8,-0.5,0.5,70,4.,1,0.5,1};
-  for (int i = 1; i < 10; ++i) {
+  TH1D *hCuts = new TH1D("hCuts","hCuts",10,0,10);
+  TNamed *taskTitle = new TNamed("taskName","deuteron3cent");
+  enum cutsName {kEtaMin=1,kEtaMax,kYMin,kYMax,kTPCsig,kTPCchi2,kSPDrec,kDCAxy,kDCAz,kRecreate};
+  double cutsA[10] = {-0.8,0.8,-0.5,0.5,70,4.,1,0.5,1,1};
+  for (int i = 1; i < 11; ++i) {
     hCuts->SetBinContent(i,cutsA[i - 1]);
   }
   gProof->AddInput(hBins);
   gProof->AddInput(hCuts);
+  gProof->AddInput(taskTitle);
   
   // Process the TDset
   gProof->Process(manual_dset, "AODSelector.cxx+g");
+  
+  // Other cuts
+  hCuts->SetBinContent(10,0.);
+  hCuts->SetBinContent(5,60);
+  taskTitle.SetTitle("deuteron3cent_chi0");
+  gProof->Process(manual_dset, "AODSelector.cxx+g");
+
 
 }
