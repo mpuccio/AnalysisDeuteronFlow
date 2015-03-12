4220f20563d91b739b7ea9785cf2cb7bace2ffb7
mpuccio <maximiliano.puccio@cern.ch>
2015-03-12 17:20:10 +0100

    Using enum to correctly set cuts

--- Spectra/SelectorTree/RunSelector.C
+++ Spectra/SelectorTree/RunSelector.C
@@ -30,7 +30,7 @@
   TH2F *hBins = new TH2F("hBins","hBins",3,kCent,28,kBins);
   TH1D *hCuts = new TH1D("hCuts","hCuts",10,0,10);
   TNamed *taskTitle = new TNamed("taskName","deuteron3cent");
-  enum cutsName {kEtaMin=1,kEtaMax,kYMin,kYMax,kTPCsig,kTPCchi2,kSPDrec,kDCAxy,kDCAz,kRecreate};
+  enum {kEtaMin=1,kEtaMax,kYMin,kYMax,kTPCsig,kTPCchi2,kSPDrec,kDCAxy,kDCAz,kRecreate};
   double cutsA[10] = {-0.8,0.8,-0.5,0.5,70,4.,1,0.5,1,1};
   for (int i = 1; i < 11; ++i) {
     hCuts->SetBinContent(i,cutsA[i - 1]);
@@ -45,54 +45,49 @@
   // Other cuts
   hCuts->SetBinContent(10,0.);
   // chi2
-  hCuts->SetBinContent(5,3.5);
+  hCuts->SetBinContent(kTPCchi2,3.5);
   taskTitle->SetTitle("deuteron3cent_chi0");
   gProof->Process(manual_dset, "AODSelector.cxx+g");
-  hCuts->SetBinContent(5,4.5);
+  hCuts->SetBinContent(kTPCchi2,4.5);
   taskTitle->SetTitle("deuteron3cent_chi1");
   gProof->Process(manual_dset, "AODSelector.cxx+g");
-  hCuts->SetBinContent(5,5);
+  hCuts->SetBinContent(kTPCchi2,5);
   taskTitle->SetTitle("deuteron3cent_chi2");
   gProof->Process(manual_dset, "AODSelector.cxx+g");
-  hCuts->SetBinContent(5,5.5);
+  hCuts->SetBinContent(kTPCchi2,5.5);
   taskTitle->SetTitle("deuteron3cent_chi3");
   gProof->Process(manual_dset, "AODSelector.cxx+g");
-  hCuts->SetBinContent(5,6.);
+  hCuts->SetBinContent(kTPCchi2,6.);
   taskTitle->SetTitle("deuteron3cent_chi4");
   gProof->Process(manual_dset, "AODSelector.cxx+g");
-  hCuts->SetBinContent(5,4.);
+  hCuts->SetBinContent(kTPCchi2,4.);
   // dca_z
-  hCuts->SetBinContent(9,0.5);
+  hCuts->SetBinContent(kDCAz,0.5);
   taskTitle->SetTitle("deuteron3cent_dcaz0");
   gProof->Process(manual_dset, "AODSelector.cxx+g");
-  hCuts->SetBinContent(9,0.75);
+  hCuts->SetBinContent(kDCAz,0.75);
   taskTitle->SetTitle("deuteron3cent_dcaz1");
   gProof->Process(manual_dset, "AODSelector.cxx+g");
-  hCuts->SetBinContent(9,1.5);
+  hCuts->SetBinContent(kDCAz,1.5);
   taskTitle->SetTitle("deuteron3cent_dcaz2");
   gProof->Process(manual_dset, "AODSelector.cxx+g");
-  hCuts->SetBinContent(9,2.);
+  hCuts->SetBinContent(kDCAz,2.);
   taskTitle->SetTitle("deuteron3cent_dcaz3");
   gProof->Process(manual_dset, "AODSelector.cxx+g");
-  hCuts->SetBinContent(9,1.);
+  hCuts->SetBinContent(kDCAz,1.);
   // tpc
-  hCuts->SetBinContent(4,60);
+  hCuts->SetBinContent(kTPCsig,60);
   taskTitle->SetTitle("deuteron3cent_tpc0");
   gProof->Process(manual_dset, "AODSelector.cxx+g");
-  hCuts->SetBinContent(4,65);
+  hCuts->SetBinContent(kTPCsig,65);
   taskTitle->SetTitle("deuteron3cent_tpc1");
   gProof->Process(manual_dset, "AODSelector.cxx+g");
-  hCuts->SetBinContent(4,75);
+  hCuts->SetBinContent(kTPCsig,75);
   taskTitle->SetTitle("deuteron3cent_tpc2");
   gProof->Process(manual_dset, "AODSelector.cxx+g");
-  hCuts->SetBinContent(4,80);
+  hCuts->SetBinContent(kTPCsig,80);
   taskTitle->SetTitle("deuteron3cent_tpc3");
   gProof->Process(manual_dset, "AODSelector.cxx+g");
-
-
-
-
-
-
+  hCuts->SetBinContent(kTPCsig,70);
 
 }
